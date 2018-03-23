291281d5923e528ad784c982abc1df718e1b6fbe
kingems <30763901+kingems@users.noreply.github.com>
2018-03-23 18:44:34 +0800

    Update guanchafulltext.js

--- guanchafulltext.js
+++ guanchafulltext.js
@@ -3,10 +3,10 @@
 // ==UserScript==
 // @name          观察者网全文显示
 // @namespace     https://github.com/kingems/gczw
-// @version       0.1.1
+// @version       0.1.2
 // @author        kingem(kingem@126.com)
 // @description   观察者网全文显示
-// @include       *://www.guancha.cn/*
+// @include       *://*.guancha.cn/*
 // @grant         GM_xmlhttpRequest
 // @run-at        document-end
 // ==/UserScript==
@@ -14,6 +14,7 @@
 // --------------------------------------------------------------------
 (function(){
     var block=document.getElementsByClassName('last');
+    var block1= document.getElementsByClassName('expand-all');
     if (block.length>1){
         var url = block[block.length-1].getAttribute("onclick");
         var start = 26;
@@ -28,6 +29,23 @@
             overrideMimeType:"text/html;charset=utf-8",
             onload: function(response) {
                 var reg=new RegExp("<div class=\"content all-txt\">([\\w\\W]*)<div class=\"share fix\">","igm");
+                var r=response.responseText.match(reg);
+                if(r!=null){
+                    contentblock.innerHTML =  r;
+                }
+            }
+       });
+    }else if (block1.length>0){
+        var url = block1[0].children[0].getAttribute("href");
+        var contentblock = document.querySelectorAll('div.article-content')[0];
+        var pageblock =document.querySelector('ul.pagination');
+        pageblock.style.display="none";
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: url,
+            overrideMimeType:"text/html;charset=utf-8",
+            onload: function(response) {
+                var reg=new RegExp("<div class=\"hot-topic-nav\"([\\w\\W]*)<div class=\"clear\"></div>","igm");
                 var r=response.responseText.match(reg);
                 if(r!=null){
                     contentblock.innerHTML =  r;
